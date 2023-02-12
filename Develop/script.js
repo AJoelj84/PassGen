445538b1d4ada6410c46339b159c0fc643aabc16
AJoelj84 <ajoelj84@gmail.com>
2023-02-11 20:49:39 -0500

    deleted several lines of code, and rewrite, would not work before, password generator works at this point

--- Develop/script.js
+++ Develop/script.js
@@ -1,55 +1,50 @@
 
-var generateBtn.addEventListener("click", writePassword);
+  var generateBtn = document.querySelector("#generate");
 
-// function for letter, number, and special character selection
-function generatePassword(){
-  var length = prompt ('Enter a password length from 8 to 128 Characters.');
-  length = parseInt (length);
-  while ( length < 8 || length > 128 || isNaN (length)){
-    length = prompt (' Please Re-Enter the desired password length from 8 to 128 Characters.');
-    length = parseInt (length);
-  }
+  generateBtn.addEventListener("click", writePassword);
 
-  var includeUpper = confirm ( 'Include Uppercase letters in your password?');
-  var includeLower = confirm ( 'Include Lowercase letters in your password?');
-  var includeSpecial = confirm ( 'Include Special Characters in your password?');
-  var includeNumbers = confirm ( 'Include Numbers in your password?');
+  function generatePassword() {
+    var length = prompt('Enter a password length from 8 to 128 Characters.');
+    length = parseInt(length);
+    while (length < 8 || length > 128 || isNaN(length)) {
+      length = prompt(' Please Re-Enter the desired password length from 8 to 128 Characters.');
+      length = parseInt(length);
+    }
 
-  var chars='';
-    if ( includeUpper){
-      chars+='ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var includeUpper = confirm('Include Uppercase letters in your password?');
+    var includeLower = confirm('Include Lowercase letters in your password?');
+    var includeSpecial = confirm('Include Special Characters in your password?');
+    var includeNumbers = confirm('Include Numbers in your password?');
+
+    var chars = '';
+    if (includeUpper) {
+      chars += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     }
-    if ( includeLower){
-      chars+='abcdefghijklmnopqrstuvwxyz'
+    if (includeLower) {
+      chars += 'abcdefghijklmnopqrstuvwxyz';
     }
-    if(includeNumbers){
-      chars+='0123456789';
+    if (includeNumbers) {
+      chars += '0123456789';
     }
-    if(includeSpecial){
-      chars+='!#$%&@?[]^*)(+,.:;=><\_`~}{'
+    if (includeSpecial) {
+      chars += '!#$%&@?[]^*)(+,.:;=><\_`~}{';
     }
-    if (chars===''){
-      alert ('At least one type must be chosen!');
+    if (chars === '') {
+      alert('At least one type must be chosen!');
       return '';
     }
 
-}
-var password ='';
-for (var i=0; i < length; i++ ){
-  password += chars.charAt (Math.floor ( Math.random() *chars.length));
-}
-  return password;
-
+    var password = '';
+    for (var i = 0; i < length; i++) {
+      password += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    return password;
+  }
 // Write password to the #password input
-function writePassword(){
-  var passwordLength = 128;
-  var includeNumbers = true;
-  var includeSpecial = true;
-  var password = generatePassword(passwordLength, includeNumbers, includeSpecial);
-  var passwordText = document.querySelector ( '#password');
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector('#password');
 
-  passwordText.value = password;
-}
-
-// Add event listener to generate button
+    passwordText.value = password;
+  }
 
