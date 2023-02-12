5fe771b2d2c7fa5afe692e010885d6e26c0f1cd1
AJoelj84 <ajoelj84@gmail.com>
2023-02-11 21:06:48 -0500

    Organizing Code and Additional Comments

--- Develop/script.js
+++ Develop/script.js
@@ -1,15 +1,15 @@
 // Tells the javascript to create an event for when the Generate button is clicked
-  var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate");
 
-  generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword);
 
 // Prompt for user to enter the desired length
   function generatePassword() {
     var length = prompt('Enter a password length from 8 to 128 Characters.');
-    length = parseInt(length);
+        length = parseInt(length);
     while (length < 8 || length > 128 || isNaN(length)) {
-      length = prompt(' Please Re-Enter the desired password length from 8 to 128 Characters.');
-      length = parseInt(length);
+        length = prompt(' Please Re-Enter the desired password length from 8 to 128 Characters.');
+        length = parseInt(length);
     }
 // Prompts for Upper and Lower Case letters as well as Special and Numbers
     var includeUpper = confirm('Include Uppercase letters in your password?');
@@ -17,24 +17,24 @@
     var includeSpecial = confirm('Include Special Characters in your password?');
     var includeNumbers = confirm('Include Numbers in your password?');
 
-    var chars = '';
-    if (includeUpper) {
-      chars += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    }
-    if (includeLower) {
-      chars += 'abcdefghijklmnopqrstuvwxyz';
-    }
-    if (includeNumbers) {
-      chars += '0123456789';
-    }
-    if (includeSpecial) {
-      chars += '!#$%&@?[]^*)(+,.:;=><\_`~}{';
-    }
-    if (chars === '') {
-      alert('At least one type must be chosen!');
+      var chars = '';
+        if (includeUpper) {
+          chars += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        }
+        if (includeLower) {
+          chars += 'abcdefghijklmnopqrstuvwxyz';
+        }
+        if (includeNumbers) {
+          chars += '0123456789';
+        }
+        if (includeSpecial) {
+          chars += '!#$%&@?[]^*)(+,.:;=><\_`~}{';
+        }
+        if (chars === '') {
+          alert('At least one type must be chosen!');
       return '';
     }
-
+// Uses input for length to determine number of characters in password
     var password = '';
     for (var i = 0; i < length; i++) {
       password += chars.charAt(Math.floor(Math.random() * chars.length));
